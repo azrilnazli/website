9fe8e2680bbf7423b5721589dd1418e8a105ae31
Azril Nazli Alias <azril.nazli@gmail.com>
2022-02-14 10:14:06 +0800

    buntu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Outlet, NavLink } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Home from "./routes/home";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    <div>
+
+      <Navbar bg="primary" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">React Bootstrap</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link as={NavLink} to="/" >Home</Nav.Link>
+                <Nav.Link as={NavLink} to="/home" >Home</Nav.Link>
+                <Nav.Link as={NavLink} to="/expenses">Expenses</Nav.Link>
+                <Nav.Link as={NavLink} to="/invoices">Invoices</Nav.Link>
+              </Nav>
+          </Container>
+      </Navbar>
+      <Outlet />
+      </div>
+
   );
 }
-
-export default App;
